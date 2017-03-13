4ee32391be7d9703e23862d08c770fc33fd9b772
Yuka Minamizaki <ym431@bath.ac.uk>
2017-03-13 20:35:53 +0000

    adding timetable

--- script.js
+++ script.js
@@ -1,14 +1,84 @@
 var canvas;
 var c;
+var canvaswidth;
+var canvasheight;
+
+function drawVerticalLine() {
+    var w = 53;
+    while (w <= 490) {
+        c.moveTo(w, 5);
+        c.lineTo(w, canvasheight);
+        c.stroke();
+        w = w + 26;
+    }
+}
+
+function drawHorizontalLine() {
+    var h = 20;
+    while (h <= 189) {
+        c.moveTo(5, h);
+        c.lineTo(canvaswidth - 5, h);
+        c.stroke();
+        h = h + 26;
+    }
+}
+
+function drawNumber() {
+    var pos = 63;
+    var number = 6;
+    var n;
+    c.font = "15px Arial";
+    while (pos <= 480) {
+        n=number.toString();
+        c.fillText(n, pos, 18);
+        pos = pos + 26;
+        number += 1;
+    }
+}
+
+function drawDay() {
+    var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    var counter = 0;
+    var pos = 38;
+    c.font = "18px Arial";
+    while (counter < 7) {
+        c.fillText(days[counter], 12, pos);
+        pos = pos + 26;
+        counter+=1;
+    }
+}
 
 function drawTable() {
+    //c.fillRect(2, 2, canvaswidth-4, canvasheight-4);
+    
+    //outline
+    c.moveTo(5, 5);
+    c.lineTo(5, canvasheight);
+    c.stroke();
+    c.moveTo(5, 5);
+    c.lineTo(canvaswidth - 5, 5);
+    c.stroke();
+    c.moveTo(canvaswidth - 5, 5);
+    c.lineTo(canvaswidth - 5, canvasheight);
+    c.stroke();
+    c.moveTo(5, canvasheight);
+    c.lineTo(canvaswidth - 5, canvasheight);
+    c.stroke();
+    
+    
+    drawVerticalLine();
+    drawHorizontalLine();
+    drawNumber();
+    drawDay();
     
 }
 
 function begin() {
 	canvas = document.getElementById("table");
 	c = canvas.getContext("2d");
-	c.lineWidth = 1.5;
+    
+    canvaswidth = c.canvas.width;
+    canvasheight = c.canvas.height;
 	
     /*
 	canvas.addEventListener('mousewheel', function (event) {
