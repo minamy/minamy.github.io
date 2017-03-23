445ef3945bb38d7fe786dc51e8c3f707b924fd12
JynxSp0ck <jynxsp0ck@googlemail.com>
2017-03-23 17:55:46 +0000

    Show Different Buttons Depending on Login Status

--- new_site/script.js
+++ new_site/script.js
@@ -6,6 +6,36 @@ function openSettings() {
     document.getElementById("usermenu").style.right = "0";
 }
 
+function logIn(){
+	var arr = document.querySelectorAll(".login");
+	for (var i = 0; i < arr.length; i++){
+		arr[i].style.paddingTop = "8px";
+		arr[i].style.paddingBottom = "8px";
+		arr[i].style.fontSize = "30px";
+	}
+	arr = document.querySelectorAll(".logout");
+	for (var i = 0; i < arr.length; i++){
+		arr[i].style.paddingTop = "0";
+		arr[i].style.paddingBottom = "0";
+		arr[i].style.fontSize = "0";
+	}
+}
+
+function logOut(){
+	var arr = document.querySelectorAll(".login");
+	for (var i = 0; i < arr.length; i++){
+		arr[i].style.paddingTop = "0";
+		arr[i].style.paddingBottom = "0";
+		arr[i].style.fontSize = "0";
+	}
+	arr = document.querySelectorAll(".logout");
+	for (var i = 0; i < arr.length; i++){
+		arr[i].style.paddingTop = "8px";
+		arr[i].style.paddingBottom = "8px";
+		arr[i].style.fontSize = "30px";
+	}
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 	document.addEventListener("click", function(e){
 		if(!(e.path.filter(function(element){
@@ -19,5 +49,6 @@ document.addEventListener("DOMContentLoaded", function(e){
 			document.getElementById("usermenu").style.right = "-250px";
 		}
 	});
+	logOut();
 });
 
