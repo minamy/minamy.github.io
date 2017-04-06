5bd897b039f879b5c5fed2b5bc087b4b4545239f
JynxSp0ck <jynxsp0ck@googlemail.com>
2017-04-07 00:58:51 +0100

    Revert "fixed login issue"
    This reverts commit 933e30ca09e6c8e09e687afc578780562b5e8aa3.

--- new_site/loadTimetable.php
+++ new_site/loadTimetable.php
@@ -3,12 +3,11 @@
         $conn = connect();
 
         if (isset($_SESSION['username'])) {
-            $query = "SELECT Timetable FROM User WHERE Username='{$_SESSION['username']}' or Email='{$_SESSION['username']}'";
+            $query = "SELECT Timetable FROM User WHERE Username='{$_SESSION['username']}'";
             $result = mysqli_query($conn, $query);
             if (mysqli_num_rows($result) > 0) {
                 $obj = mysqli_fetch_object($result);
-                $obj = $obj->Timetable;
-                $timetable = unserialize($obj);
+                $timetable = unserialize($obj->Timetable);
                 echo json_encode($timetable);
             } else {
                 echo "fail";
@@ -27,5 +26,4 @@
             }
             return  $connect;
         }
-
 ?>
